a17305f3946e4a5e8649a167435eecaf1b36e7aa
Hydraxuz <96903828+Hydraxuz@users.noreply.github.com>
2025-05-17 01:52:49 -0400

    Dev - FPSlmiter Script
    Set it to 60fps

---
+++ Assets/Scripts/FPSlimiter.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class FPSlimiter : MonoBehaviour
+{
+    private int targetFPS = 60;
+
+    void Awake()
+    {
+        Application.targetFrameRate = targetFPS;
+        QualitySettings.vSyncCount = 0; // Disable VSync to let Application.targetFrameRate work
+    }
+
+    void OnValidate()
+    {
+        // Update frame rate in the editor when value changes
+        Application.targetFrameRate = targetFPS;
+    }
+}
